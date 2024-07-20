84b2b6d5d80ae8e38bca53f629758d637fa977ce
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-07-20 00:36:08 -0600

    change to use dockerhub

--- obj/Debug/net8.0/ReversedTetrisApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ReversedTetrisApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReversedTetrisApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d545c8cc541ae10d2cb1288366bb93d9f9d5acb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d93648825ff4601ac0cca75b4336678b33ba7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReversedTetrisApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReversedTetrisApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
